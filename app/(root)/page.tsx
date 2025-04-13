422ff197737ba9909c30885fe81a3699710e607d
Waseemkhan950 <waseemadil950@gmail.com>
2025-04-14 01:35:49 +0500

    basic setup of the project using shadcn and tailwind customization

---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+	return <Button className="bg-red-500">Click Me</Button>;
+};
+
+export default Home;
---
+++ lib/constants/index.ts
@@ -0,0 +1,4 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Zayrah";
+export const APP_DESCRIPTION = "Zayrah is an ecommerce platform for clothing.";
+export const SERVER_URL =
+	process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
